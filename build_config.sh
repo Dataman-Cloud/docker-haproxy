3ecf54793d72456a05b1824f476396bb4ad993bf
ss75710541 <75710541@qq.com>
2017-09-20 12:09:46 +0800

    add BACKEND_PORT HAPROXY_STATS_PORT

--- build_config.sh
+++ build_config.sh
@@ -7,11 +7,13 @@ BACKEND_LIST=$(echo $BACKEND_LIST |sed "s/,/ /g")
 BACKEND_MAXCONN=${BACKEND_MAXCONN:-20}
 SERVICE_NAME=${SERVICE_NAME:-testserver}
 SERVICE_PORT=${SERVICE_PORT:-80}
+BACKEND_PORT=${BACKEND_PORT:-80}
+HAPROXY_STATS_PORT=${HAPROXY_STATS_PORT:-9000}
 SERVERS=""
 
 num=1
 for SERVER_IP in $BACKEND_LIST;do
-    SERVERS="$SERVERS server $SERVICE_NAME-$SERVER_IP:$SERVICE_PORT $SERVER_IP:$SERVICE_PORT check port $SERVICE_PORT maxconn $BACKEND_MAXCONN\n"
+    SERVERS="$SERVERS server $SERVICE_NAME-$SERVER_IP:$BACKEND_PORT $SERVER_IP:$BACKEND_PORT check port $BACKEND_PORT maxconn $BACKEND_MAXCONN\n"
     num=`expr $num + 1`
 done
 
@@ -19,6 +21,8 @@ replace_var(){
     files=$@
     echo $files | xargs sed -i 's/--SERVICE_NAME--/'"$SERVICE_NAME"'/g'
     echo $files | xargs sed -i 's/--SERVICE_PORT--/'"$SERVICE_PORT"'/g'
+    echo $files | xargs sed -i 's/--BACKEND_PORT--/'"$BACKEND_PORT"'/g'
+    echo $files | xargs sed -i 's/--HAPROXY_STATS_PORT--/'"$HAPROXY_STATS_PORT"'/g'
     echo $files | xargs sed -i 's/--SERVERS--/'"$SERVERS"'/g'
 }
 
--- run.sh
+++ run.sh
@@ -2,7 +2,9 @@ docker run -d \
 	-e BACKEND_LIST=192.168.1.221,192.168.1.222,192.168.1.223 \
 	-e BACKEND_MAXCONN=20 \
 	-e SERVICE_NAME=test \
-	-e SERVICE_PORT=5013 \
+	-e SERVICE_PORT=7001 \
+	-e BACKEND_PORT=7001 \
+	-e HAPROXY_STATS_PORT=9001 \
 	-v /var/run/haproxy:/run/haproxy \
 	--net host \
 	library/alpine3.4-haproxy-v1.6.9
